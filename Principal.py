81e3affa91f577f7b8c87ae6b3992449c4b9f5b9
gui9394 <gui9394@gmail.com>
2018-09-18 11:19:03 -0300

    Comentários e correções

--- Principal.py
+++ Principal.py
@@ -1,62 +1,99 @@
 from PIL import Image
 
 
-def pixels_vizinhos(imagem: Image, xy:tuple):
-    pixels = list()
+def get_pixels_vizinhos(imagem, xy):
+    """
+    Retorna uma lista com os valores dos pixel entorno das coordenadas 'x y', incluindo ele.
+    :param imagem: Image
+    :param xy: tuple
+    :return: list
+    """
+
+    x = xy[0]
+    y = xy[1]
+
     x_max = imagem.size[0]
     y_max = imagem.size[1]
 
-    for x in range(xy[0]-1, xy[0]+2):
-        for y in range(xy[1]-1, xy[1]+2):
-            if ((x > -1) & (x < x_max) & (y > -1) & (y < y_max)):
-                pixels.append(imagem.getpixel((x, y)))
+    lista = list()
 
-    return pixels
+    for x_temp in range(x - 1, x + 2):
+        for y_temp in range(y - 1, y + 2):
+            if ((x_temp > -1) & (x_temp < x_max) & (y_temp > -1) & (y_temp < y_max)):
+                coordenadas = (x_temp, y_temp)
+                lista.append(imagem.getpixel(coordenadas))
+
+    return lista
 
 
-def filtro_mediana(imagem:Image):
+def filtro_mediana(imagem: Image):
+    """
+    Retorna a imagem com o filtro de mediana aplicado.
+    :param imagem: Image
+    :return: Image
+    """
+
     def mediana(lista:list):
-        lista.sort()
+        """
+        Retorna a mediana de uma lista de valores.
+        :param lista: list
+        :return: int
+        """
+
+        lista.sort() # ordena lista de forma crescente
         tamanho_lista = len(lista)
 
+        # Lista com quantidade de itens par
         if (tamanho_lista % 2) == 0:
             return lista[tamanho_lista // 2]
+        # Lista com quantidade de itens impar
         else:
-            meio_0 = lista[tamanho_lista // 2]
-            meio_1 = lista[(tamanho_lista // 2) + 1]
-            return (meio_0 + meio_1) // 2
+            idx = tamanho_lista // 2
+            return (lista[idx] + lista[idx + 1]) // 2
 
-    nova_imagem = Image.new("L", imagem.size)
+    nova_imagem = Image.new("L", imagem.size) # nova imagem
 
     for x in range(imagem.size[0]):
         for y in range(imagem.size[1]):
-            nova_imagem.putpixel((x, y), mediana(pixels_vizinhos(imagem, (x, y))))
+            coodernadas = (x, y)
+            pixels_vizinhos = get_pixels_vizinhos(imagem, coodernadas)
+            nova_imagem.putpixel(coodernadas, mediana(pixels_vizinhos))
 
     return nova_imagem
 
 
-def filtro_media(imagem:Image):
+def filtro_media(imagem: Image):
+    """
+    Retorna a imagem com o filtro media aplicado.
+    :param imagem: Image
+    :return: Image
+    """
+
     def media(lista:list):
+        """
+        Retorna media dos valores dos itens de uma lista(sem casas decimais).
+        :param lista: list
+        :return: int
+        """
         return (sum(lista)//len(lista))
 
-    nova_imagem = Image.new("L", imagem.size)
+    nova_imagem = Image.new("L", imagem.size) # nova imagem
 
     for x in range(imagem.size[0]):
         for y in range(imagem.size[1]):
-            nova_imagem.putpixel((x, y), media(pixels_vizinhos(imagem, (x, y))))
+            coodernadas = (x, y)
+            pixels_vizinhos = get_pixels_vizinhos(imagem, coodernadas)
+            nova_imagem.putpixel(coodernadas, media(pixels_vizinhos))
 
     return nova_imagem
 
-
+# Carregando imagem
 img = Image.open("0.jpeg")
 
+# Aplicando filtro media
 img0 = filtro_media(img)
-img0.save("media.png", "png")
+img0.save("out_media.png", "png")
 
+# Aplicando filtro mediana
 img1 = filtro_mediana(img)
-img1.save("mediana.png", "png")
-
-
-"""
-histograma com grafico
-"""
+img1.save("out_mediana.png", "png")
